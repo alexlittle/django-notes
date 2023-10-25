101c87043160711ad89f75482481da934f15d5e1
Alex Little <consult@alexlittle.net>
2023-10-25 11:24:22 +0200

    improved cleaning

--- bookmark/management/commands/clean_tags.py
+++ bookmark/management/commands/clean_tags.py
@@ -3,6 +3,7 @@ Removes unused tags
 '''
 
 from django.core.management.base import BaseCommand
+from django.db.models import Count
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -10,18 +11,18 @@ from bookmark.models import Bookmark, Tag
 
 
 class Command(BaseCommand):
-    help = _(u"Checks the urls to ensure they are still valid links")
+    help = _(u"Cleans unused tags and flags any that are only used once")
     errors = []
 
     def handle(self, *args, **options):
-        tags = Tag.objects.filter(favourite=False).order_by('name')
-        for tag in tags:
-            if tag.bookmark_count() == 0:
-                print(tag.name + ": deleted")
-                tag.delete()
+        tags = Tag.objects.filter(favourite=False).annotate(bmcount=Count("bookmarktag"))
 
-        for tag in tags:
-            if tag.bookmark_count() == 1:
-                bookmark = Bookmark.objects.get(bookmarktag__tag=tag)
-                print(tag.name + ": only 1 use : http://localhost.bookmark"
-                      + reverse('bookmark:edit', args=[bookmark.id]))
+        unused_tags = tags.filter(bmcount=0)
+        for tag in unused_tags:
+            print(tag.name + ": deleted")
+            tag.delete()
+
+        used_once_tags = tags.filter(bmcount=1).order_by("name")
+        for idx, tag in enumerate(used_once_tags):
+            url = "http://localhost:9030" + reverse('bookmark:tag_view', args=[tag.slug])
+            print("%d/%d %s: only 1 use : %s" % (idx, used_once_tags.count(), tag.name, url))
