65ef919d07b0a75383b4b1a18c7aa55bdca2cf1b
Alex Little <consult@alexlittle.net>
2023-11-03 15:04:41 +0200

    updates

--- bookmark/management/commands/link_checker.py
+++ bookmark/management/commands/link_checker.py
@@ -7,8 +7,7 @@ import datetime
 import http
 import socket
 import ssl
-import urllib.error
-import urllib.request
+from urllib import request, error
 
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext_lazy as _
@@ -16,6 +15,10 @@ from django.utils import timezone
 
 from bookmark.models import Bookmark
 
+
+class NoRedirect(request.HTTPRedirectHandler):
+    def redirect_request(self, req, fp, code, msg, headers, newurl):
+        return None
 
 class Command(BaseCommand):
     help = _(u"Checks the urls to ensure they are still valid links")
@@ -35,32 +38,35 @@ class Command(BaseCommand):
             bookmarks = Bookmark.objects.filter(link_check_date__lte=today_minus_days)
 
         error_list = []
+        redirect_list = []
 
         for idx, bookmark in enumerate(bookmarks):
             print("Checking: %s (%d/%d)" % (bookmark.url, idx, len(bookmarks)))
-
+            opener = request.build_opener(NoRedirect)
+            request.install_opener(opener)
             try:
-                request = urllib.request.Request(
+                my_request = request.Request(
                     bookmark.url,
-                    data=None,
+                    method="GET",
                     headers={
                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
                     }
                 )
-                response = urllib.request.urlopen(request, timeout=20)
+                response = request.urlopen(my_request, timeout=20)
                 print(response.code)
-                self.update_link_check_ok(bookmark)
-            except (urllib.error.HTTPError,
-                    urllib.error.URLError,
-                    ssl.CertificateError,
+                self.update_link_check(bookmark, "ok")
+            except (ssl.CertificateError,
                     http.client.RemoteDisconnected,
                     ConnectionResetError,
                     socket.timeout,
                     http.client.BadStatusLine):
-                bookmark.link_check_date = timezone.now()
-                bookmark.link_check_result = "error"
-                bookmark.save()
+                print("Error")
+                self.update_link_check(bookmark, "error")
                 error_list.append(bookmark)
+            except (error.URLError, error.HTTPError):
+                print("has been redirected")
+                self.update_link_check(bookmark, "redirect")
+                redirect_list.append(bookmark)
 
         print("%d errors" % len(error_list))
         for idx, el in enumerate(error_list):
@@ -69,7 +75,11 @@ class Command(BaseCommand):
             if accept == 'y':
                 el.delete()
 
-    def update_link_check_ok(self, bookmark):
+        print("%d redirects" % len(redirect_list))
+        for idx, rl in enumerate(redirect_list):
+            print("%d/%d %s" % (idx, len(redirect_list), rl.url))
+
+    def update_link_check(self, bookmark, result):
         bookmark.link_check_date = timezone.now()
-        bookmark.link_check_result = "ok"
+        bookmark.link_check_result = result
         bookmark.save()
