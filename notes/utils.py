c7eb66418cebaf9852f61cf131c3d297a320425d
Alex Little <consult@alexlittle.net>
2025-03-29 19:55:13 +0200

    update for timezone

--- notes/utils.py
+++ notes/utils.py
@@ -31,6 +31,32 @@ def get_user_aware_date(user):
         else:
             return datetime.datetime.now().date()
 
+def get_user_aware_datetime(user):
+    """
+    Returns a datetime.datetime.now() object that is aware of the user's timezone.
+
+    Args:
+        user: A Django user object.
+
+    Returns:
+        A datetime.datetime object, or None if the user or timezone is invalid.
+    """
+
+    if not user.is_authenticated:
+        return timezone.now()
+
+    try:
+        user_timezone = user.profile.timezone
+        if isinstance(user_timezone, str):
+            user_timezone = pytz.timezone(user_timezone)
+        return datetime.datetime.now(user_timezone)
+
+    except (AttributeError, pytz.exceptions.UnknownTimeZoneError):
+        if settings.USE_TZ:
+            return timezone.now()
+        else:
+            return datetime.datetime.now()
+
 
 def is_showall(request):
     showall_str = request.GET.get('showall', 'false').lower()
