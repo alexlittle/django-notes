93d05fba6498c2f9e8f5e1b5dc485962ed1ba7c2
Alex Little <consult@alexlittle.net>
2025-04-19 08:48:45 +0100

    add tag diff

--- notes/management/commands/tag_diff.py
+++ notes/management/commands/tag_diff.py
@@ -10,7 +10,12 @@ class Command(BaseCommand):
     help = _(u"Check for tags that are very similar")
     errors = []
 
+    def add_arguments(self, parser):
+        parser.add_argument('cutoff', type=float, default=0.6, nargs='?')
+
     def handle(self, *args, **options):
+
+        cutoff = options['cutoff']
 
         # get all tags as plain list
         tags = list(Tag.objects.order_by('name').values_list('name', flat=True))
@@ -18,7 +23,7 @@ class Command(BaseCommand):
         for current_tag in tags:
 
             filtered_list = [tag for tag in tags if tag != current_tag]
-            matches = difflib.get_close_matches(current_tag, filtered_list, cutoff=0.7)
+            matches = difflib.get_close_matches(current_tag, filtered_list, cutoff=cutoff)
             if matches:
                 print(current_tag)
                 print(matches)
