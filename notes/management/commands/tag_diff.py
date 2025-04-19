1f2efdb935236a59ab45a7b955bc53259c154fda
Alex Little <consult@alexlittle.net>
2025-04-19 08:40:35 +0100

    add tag diff

--- notes/management/commands/tag_diff.py
+++ notes/management/commands/tag_diff.py
@@ -1,5 +1,6 @@
+import difflib
+
 from django.core.management.base import BaseCommand
-from django.db.models import Count
 from django.utils.translation import gettext_lazy as _
 
 from notes.models import Tag
@@ -10,4 +11,15 @@ class Command(BaseCommand):
     errors = []
 
     def handle(self, *args, **options):
-        pass
+
+        # get all tags as plain list
+        tags = list(Tag.objects.order_by('name').values_list('name', flat=True))
+
+        for current_tag in tags:
+
+            filtered_list = [tag for tag in tags if tag != current_tag]
+            matches = difflib.get_close_matches(current_tag, filtered_list, cutoff=0.7)
+            if matches:
+                print(current_tag)
+                print(matches)
+                print("----------------")
